dc2204ab5ff87c7780aa69e6d8ad33999153eb1c
kingerburg <vancel92@yandex.by>
2022-05-24 17:15:22 +0300

    fix: extname file

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,13 +6,12 @@ const folder = path.join(__dirname, 'secret-folder');
 fs.readdir(folder, {withFileTypes: true}, (err, dirEntry) => {
     if (!err) {
         dirEntry.forEach((dirEntryList) => {
-            let fileName = dirEntryList.name.split('.')[0];
-            let fileExs = dirEntryList.name.split('.')[1];
+            let fileName = path.basename(dirEntryList.name, path.extname(dirEntryList.name));
+            let fileExs = path.extname(dirEntryList.name).replace('.','');
              if (dirEntryList.isFile() && fileExs != undefined) {
                 fs.stat(`${folder}/${dirEntryList.name}`, (err, call) => {
                     let fileByte = call.size;
                 console.log(`${fileName} - ${fileExs} - ${fileByte} byte`);
-
                 })
             }
         })
